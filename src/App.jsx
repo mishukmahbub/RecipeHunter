d343a559f1e210e05e1fdafc429d4acc35dd69ea
mishukmahbub <mishukmahbub@gmail.com>
2023-05-02 08:49:03 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <button className="btn">Button</button>
+    </div>
+  );
+};
+
+export default App;

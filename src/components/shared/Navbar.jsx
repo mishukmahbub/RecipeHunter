e9399a5a206875531829028b507f77154b3c46af
mishukmahbub <mishukmahbub@gmail.com>
2023-05-04 19:47:11 +0600

    user pic updated

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                         <div className="dropdown dropdown-end">
                             <label tabIndex={0} className="btn btn-ghost btn-circle avatar tooltip  tooltip-bottom" data-tip={user.displayName}>
                                 <div className="w-10 rounded-full">
-                                    <img src="http://placebeard.it/640x480" />
+                                    <img src={user.photoURL} />
                                 </div>
                             </label>
                             <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
